f4a1fd699639f597aad74cfcc38e1ec5f6801248
derangel <ofitserovd@gmail.com>
2021-03-09 14:26:34 +0300

    ipv6 genrange method

--- src/main/java/IPgen/IPv6Generator.java
+++ src/main/java/IPgen/IPv6Generator.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class IPv6Generator extends IPv4Generator {
 
@@ -92,6 +93,276 @@ public class IPv6Generator extends IPv4Generator {
         } catch (IOException var5) {
             var5.printStackTrace();
         }
+    }
+
+    @Override
+    public void genRange (String firstIP, String lastIP) {
+        int[] fIP = parseIP(firstIP);
+        int[] lIP = parseIP(lastIP);
+        try {
+            FileWriter fileWriter = new FileWriter(this.IPList, append);
+            BufferedWriter bf = new BufferedWriter(fileWriter);
+            //--------------------------------------------------------------------------------------------
+            int i = 0;
+            int i0=fIP[0], i1=fIP[1], i2=fIP[2], i3=fIP[3], i4=fIP[4], i5=fIP[5], i6=fIP[6], i7=fIP[7];
+            while (i0 < lIP[0]) {
+                while (i1 <= 65535) {
+                    while (i2 <= 65535) {
+                        while (i3 <= 65535) {
+                            while (i4 <= 65535) {
+                                while (i5 <= 65535) {
+                                    while (i6 <= 65535) {
+                                        while (i7 <= 65535) {
+                                            bf.write("IP" + (IPN+i) + ";" +
+                                                    String.format("%X", i0) + ":" +
+                                                    String.format("%X", i1) + ":" +
+                                                    String.format("%X", i2) + ":" +
+                                                    String.format("%X", i3) + ":" +
+                                                    String.format("%X", i4) + ":" +
+                                                    String.format("%X", i5) + ":" +
+                                                    String.format("%X", i6) + ":" +
+                                                    String.format("%X", i7) + "\n");
+                                            if (i % 1000000 == 0) {
+                                                bf.flush();
+                                            }
+                                            i++;
+                                            i7++;
+                                        }
+                                        i7 = 0;
+                                        i6++;
+                                    }
+                                    i6 = 0;
+                                    i5++;
+                                }
+                                i5 = 0;
+                                i4++;
+                            }
+                            i4 = 0;
+                            i3++;
+                        }
+                        i3 = 0;
+                        i2++;
+                    }
+                    i2 = 0;
+                    i1++;
+                }
+                i1 = 0;
+                i0++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i1 < lIP[1]) {
+                while (i2 <= 65535) {
+                    while (i3 <= 65535) {
+                        while (i4 <= 65535) {
+                            while (i5 <= 65535) {
+                                while (i6 <= 65535) {
+                                    while (i7 <= 65535) {
+                                        bf.write("IP" + (IPN+i) + ";" +
+                                                String.format("%X", i0) + ":" +
+                                                String.format("%X", i1) + ":" +
+                                                String.format("%X", i2) + ":" +
+                                                String.format("%X", i3) + ":" +
+                                                String.format("%X", i4) + ":" +
+                                                String.format("%X", i5) + ":" +
+                                                String.format("%X", i6) + ":" +
+                                                String.format("%X", i7) + "\n");
+                                        if (i % 1000000 == 0) {
+                                            bf.flush();
+                                        }
+                                        i++;
+                                        i7++;
+                                    }
+                                    i7 = 0;
+                                    i6++;
+                                }
+                                i6 = 0;
+                                i5++;
+                            }
+                            i5 = 0;
+                            i4++;
+                        }
+                        i4 = 0;
+                        i3++;
+                    }
+                    i3 = 0;
+                    i2++;
+                }
+                i2 = 0;
+                i1++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i2 < lIP[2]) {
+                while (i3 <= 65535) {
+                    while (i4 <= 65535) {
+                        while (i5 <= 65535) {
+                            while (i6 <= 65535) {
+                                while (i7 <= 65535) {
+                                    bf.write("IP" + (IPN+i) + ";" +
+                                            String.format("%X", i0) + ":" +
+                                            String.format("%X", i1) + ":" +
+                                            String.format("%X", i2) + ":" +
+                                            String.format("%X", i3) + ":" +
+                                            String.format("%X", i4) + ":" +
+                                            String.format("%X", i5) + ":" +
+                                            String.format("%X", i6) + ":" +
+                                            String.format("%X", i7) + "\n");
+                                    if (i % 1000000 == 0) {
+                                        bf.flush();
+                                    }
+                                    i++;
+                                    i7++;
+                                }
+                                i7 = 0;
+                                i6++;
+                            }
+                            i6 = 0;
+                            i5++;
+                        }
+                        i5 = 0;
+                        i4++;
+                    }
+                    i4 = 0;
+                    i3++;
+                }
+                i3 = 0;
+                i2++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i3 < lIP[3]) {
+                while (i4 <= 65535) {
+                    while (i5 <= 65535) {
+                        while (i6 <= 65535) {
+                            while (i7 <= 65535) {
+                                bf.write("IP" + (IPN+i) + ";" +
+                                        String.format("%X", i0) + ":" +
+                                        String.format("%X", i1) + ":" +
+                                        String.format("%X", i2) + ":" +
+                                        String.format("%X", i3) + ":" +
+                                        String.format("%X", i4) + ":" +
+                                        String.format("%X", i5) + ":" +
+                                        String.format("%X", i6) + ":" +
+                                        String.format("%X", i7) + "\n");
+                                if (i % 1000000 == 0) {
+                                    bf.flush();
+                                }
+                                i++;
+                                i7++;
+                            }
+                            i7 = 0;
+                            i6++;
+                        }
+                        i6 = 0;
+                        i5++;
+                    }
+                    i5 = 0;
+                    i4++;
+                }
+                i4 = 0;
+                i3++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i4 < lIP[4]) {
+                while (i5 <= 65535) {
+                    while (i6 <= 65535) {
+                        while (i7 <= 65535) {
+                            bf.write("IP" + (IPN+i) + ";" +
+                                    String.format("%X", i0) + ":" +
+                                    String.format("%X", i1) + ":" +
+                                    String.format("%X", i2) + ":" +
+                                    String.format("%X", i3) + ":" +
+                                    String.format("%X", i4) + ":" +
+                                    String.format("%X", i5) + ":" +
+                                    String.format("%X", i6) + ":" +
+                                    String.format("%X", i7) + "\n");
+                            if (i % 1000000 == 0) {
+                                bf.flush();
+                            }
+                            i++;
+                            i7++;
+                        }
+                        i7 = 0;
+                        i6++;
+                    }
+                    i6 = 0;
+                    i5++;
+                }
+                i5 = 0;
+                i4++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i5 < lIP[5]) {
+                while (i6 <= 65535) {
+                    while (i7 <= 65535) {
+                        bf.write("IP" + (IPN+i) + ";" +
+                                String.format("%X", i0) + ":" +
+                                String.format("%X", i1) + ":" +
+                                String.format("%X", i2) + ":" +
+                                String.format("%X", i3) + ":" +
+                                String.format("%X", i4) + ":" +
+                                String.format("%X", i5) + ":" +
+                                String.format("%X", i6) + ":" +
+                                String.format("%X", i7) + "\n");
+                        if (i % 1000000 == 0) {
+                            bf.flush();
+                        }
+                        i++;
+                        i7++;
+                    }
+                    i7 = 0;
+                    i6++;
+                }
+                i6 = 0;
+                i5++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i6 < lIP[6]) {
+                while (i7 <= 65535) {
+                    bf.write("IP" + (IPN+i) + ";" +
+                            String.format("%X", i0) + ":" +
+                            String.format("%X", i1) + ":" +
+                            String.format("%X", i2) + ":" +
+                            String.format("%X", i3) + ":" +
+                            String.format("%X", i4) + ":" +
+                            String.format("%X", i5) + ":" +
+                            String.format("%X", i6) + ":" +
+                            String.format("%X", i7) + "\n");
+                    if (i % 1000000 == 0) {
+                        bf.flush();
+                    }
+                    i++;
+                    i7++;
+                }
+                i7 = 0;
+                i6++;
+            }
+            //--------------------------------------------------------------------------------------------
+            while (i7 <= lIP[7]) {
+                bf.write("IP" + (IPN+i) + ";" +
+                        String.format("%X", i0) + ":" +
+                        String.format("%X", i1) + ":" +
+                        String.format("%X", i2) + ":" +
+                        String.format("%X", i3) + ":" +
+                        String.format("%X", i4) + ":" +
+                        String.format("%X", i5) + ":" +
+                        String.format("%X", i6) + ":" +
+                        String.format("%X", i7) + "\n");
+                if (i % 1000000 == 0) {
+                    bf.flush();
+                }
+                i++;
+                i7++;
+            }
+
+            bf.flush();
+            bf.close();
+        } catch (IOException var5) {
+            var5.printStackTrace();
+        }
+    }
+
+    @Override
+    public void genFullSubnet (String IP, String netmask) {
+        System.out.println("netmask is not supported for IPv6");
     }
 
 
